44269e473867e04594055c47283f3b7fc5da7d06
Masud Rana <mranasrj@gmail.com>
2022-06-22 23:57:10 +0600

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/shared/Header';
+import Sidebar from './components/shared/Sidebar';
+import Footer from './components/shared/Footer';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header/>
+      <Sidebar/>
+      <Dashboard/>
+      <Footer/>
     </div>
   );
 }
